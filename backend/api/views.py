29ca4411bf8cfe7c68fb3243e5203676b74fd1a5
github username <krishel.lasam@gmail.com>
2017-12-09 13:20:49 -0500

    create customer view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import CustomerSerializer
+from .models import Customer
 
-# Create your views here.
+class CreateCustomerView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+    def perform_customer_create(self, serializer):
+        """Save the post data when creating a new customer."""
+        serializer.save()
