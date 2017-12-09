86bb39c69a897dead50bb5f0b37f0e3a7693cf76
github username <krishel.lasam@gmail.com>
2017-12-09 13:57:02 -0500

    add new customer details view url association

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateCustomerView
+from .views import CreateCustomerView, CustomerDetailsView
 
 urlpatterns = {
-    url(r'^customers/$', CreateCustomerView.as_view(), name="create"),
+    url(r'^customers/$', CreateCustomerView.as_view(), name="custCreate"),
+    url(r'^customers/(?P<pk>[0-9]+)/$', CustomerDetailsView.as_view(), name="custDetails"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
