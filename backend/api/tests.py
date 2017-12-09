ef64656191cb5b27d30f5564fd3ceb55c3388222
github username <krishel.lasam@gmail.com>
2017-12-09 13:54:15 -0500

    add tests for customer get, put, delete

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -26,10 +26,40 @@ class CustomerViewTestCase(TestCase):
         self.client = APIClient()
         self.customer_data = {'first_name': 'First', 'last_initial': 'L'}
         self.response = self.client.post(
-            reverse('create'),
+            reverse('custCreate'),
             self.customer_data,
             format="json")
 
     def test_api_can_create_a_customer(self):
         """Test the api has customer creation capability."""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_can_get_a_customer(self):
+        """Test the api can get a given customer."""
+        customer = Customer.objects.get()
+        response = self.client.get(
+            reverse('custDetails',
+            kwargs={'pk': customer.id}), format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, customer)
+
+    def test_api_can_update_customer(self):
+        """Test the api can update a given customer."""
+        customer = Customer.objects.get()        
+        update_customer = {'first_name': 'Another', 'last_initial': 'K'}
+        response = self.client.put(
+            reverse('custDetails', kwargs={'pk': customer.id}),
+            update_customer, format='json')
+            
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_customer(self):
+        """Test the api can delete a customer."""
+        customer = Customer.objects.get()
+        response = self.client.delete(
+            reverse('custDetails', kwargs={'pk': customer.id}),
+            format='json',
+            follow=True)
+
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
