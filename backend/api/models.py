0a38ecfbcd1f4511c7464197977aaff0b36bbe7c
github username <krishel.lasam@gmail.com>
2017-12-09 13:22:48 -0500

    add letters-only validator to models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 class Customer(models.Model):
     """This class represents the customer model."""
-    first_name = models.CharField(max_length=255, blank=False, unique=False)
-    last_initial = models.CharField(max_length=1, blank=False, unique=False)
+    letters = RegexValidator(r'^[a-zA-Z]*$', 'Only letters are allowed.')
+    first_name = models.CharField(max_length=255, blank=False, unique=False, validators=[letters])
+    last_initial = models.CharField(max_length=1, blank=False, unique=False, validators=[letters])
 
     def __str__(self):
         """Return a human readable representation of the model instance."""
