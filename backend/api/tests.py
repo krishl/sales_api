a67be001a2c051f6506aeec980166776436a032b
github username <krishel.lasam@gmail.com>
2017-12-09 12:36:20 -0500

    migrate Customer

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Customer
 
-# Create your tests here.
+class CustomerTestCase(TestCase):
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.first_name = "First name"
+        self.last_initial = "L"
+        self.customer = Customer(first_name=self.first_name, last_initial=self.last_initial)
+
+    def test_model_can_create_a_customer(self):
+        """Test the customer model can create a customer."""
+        old_count = Customer.objects.count()
+        self.customer.save()
+        new_count = Customer.objects.count()
+        self.assertNotEqual(old_count, new_count)
