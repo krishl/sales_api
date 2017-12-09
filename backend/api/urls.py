5b4728347c58f048ed382664cb72f617b4976a6a
github username <krishel.lasam@gmail.com>
2017-12-09 13:21:20 -0500

    create initial urls.py

---
+++ backend/api/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import CreateCustomerView
+
+urlpatterns = {
+    url(r'^customers/$', CreateCustomerView.as_view(), name="create"),
+}
+
+urlpatterns = format_suffix_patterns(urlpatterns)
