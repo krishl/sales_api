3f15b6cd4728efb37402d9ab94447459a9bef978
github username <krishel.lasam@gmail.com>
2017-12-09 13:20:15 -0500

    create test for customer create api

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
 from .models import Customer
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
 
-class CustomerTestCase(TestCase):
+class CustomerModelTestCase(TestCase):
     def setUp(self):
         """Define the test client and other test variables."""
-        self.first_name = "First name"
+        self.first_name = "First"
         self.last_initial = "L"
         self.customer = Customer(first_name=self.first_name, last_initial=self.last_initial)
 
@@ -14,3 +17,19 @@ class CustomerTestCase(TestCase):
         self.customer.save()
         new_count = Customer.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+class CustomerViewTestCase(TestCase):
+    """Test suite for the api views."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.client = APIClient()
+        self.customer_data = {'first_name': 'First', 'last_initial': 'L'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.customer_data,
+            format="json")
+
+    def test_api_can_create_a_customer(self):
+        """Test the api has customer creation capability."""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
