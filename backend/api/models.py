b45bb2e612cda6c9f6b11edbd49072dadb266d44
github username <krishel.lasam@gmail.com>
2017-12-09 12:31:05 -0500

    create initial customer model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    """This class represents the customer model."""
+    first_name = models.CharField(max_length=255, blank=False, unique=False)
+    last_initial = models.CharField(max_length=1, blank=False, unique=False)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return "%s the place" % self.first_name
