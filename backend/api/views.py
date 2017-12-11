20c1739a7b097e960f0cdc549b77771395e5e8b2
github username <krishel.lasam@gmail.com>
2017-12-11 01:28:00 -0500

    create product views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializers import CustomerSerializer
-from .models import Customer
+from .serializers import CustomerSerializer, ProductSerializer
+from .models import Customer, Product
 
 class CreateCustomerView(generics.ListCreateAPIView):
     """This class defines the create behavior of our rest api."""
@@ -17,3 +17,18 @@ class CustomerDetailsView(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+class CreateProductView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_customer_create(self, serializer):
+        """Save the post data when creating a new customer."""
+        serializer.save()
+
+class ProductDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class handles the http GET, PUT and DELETE requests."""
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
