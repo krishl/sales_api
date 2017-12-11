5f0da859a09d583ae696a7ea3480afeed5733b1f
github username <krishel.lasam@gmail.com>
2017-12-11 02:08:48 -0500

    create cart model and migrate

--- backend/api/models.py
+++ backend/api/models.py
@@ -10,11 +10,20 @@ class Customer(models.Model):
     def __str__(self):
         """Return a human readable representation of the model instance."""
         return "%s" % self.first_name
-    
+
+class Cart(models.Model):
+    """This class represents the cart model."""
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return "%s" % self.customer
+
 class Product(models.Model):
     """This class represents the product model."""
     name = models.CharField(max_length=255, blank=False, unique=False)
     description = models.CharField(max_length=1000, blank=False, unique=False)
+    cart = models.ForeignKey(Cart, on_delete=models.PROTECT)
 
     def __str__(self):
         """Return a human readable representation of the model instance."""
