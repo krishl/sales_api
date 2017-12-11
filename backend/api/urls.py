28a9cd560cc1f60623263fe9c880612e2ac5f7cc
github username <krishel.lasam@gmail.com>
2017-12-11 01:27:42 -0500

    create product url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateCustomerView, CustomerDetailsView
+from .views import CreateCustomerView, CustomerDetailsView, CreateProductView, ProductDetailsView
 
 urlpatterns = {
     url(r'^customers/$', CreateCustomerView.as_view(), name="custCreate"),
     url(r'^customers/(?P<pk>[0-9]+)/$', CustomerDetailsView.as_view(), name="custDetails"),
+    url(r'^products/$', CreateProductView.as_view(), name="prodCreate"),
+    url(r'^products/(?P<pk>[0-9]+)/$', ProductDetailsView.as_view(), name="prodDetails"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
