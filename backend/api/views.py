dbaeba5c61e840c423cd6847a9e206774ee4e610
github username <krishel.lasam@gmail.com>
2017-12-12 00:28:03 -0500

    add cart views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializers import CustomerSerializer, ProductSerializer
-from .models import Customer, Product
+from .serializers import CustomerSerializer, ProductSerializer, CartSerializer
+from .models import Customer, Product, Cart
 
 class CreateCustomerView(generics.ListCreateAPIView):
     """This class defines the create behavior of our rest api."""
@@ -32,3 +32,26 @@ class ProductDetailsView(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class CreateCartView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+    def perform_customer_create(self, serializer):
+        """Save the post data when creating a new customer."""
+        serializer.save()
+
+class CartDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class handles the http GET, PUT and DELETE requests."""
+
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+class CustomerCartView(generics.ListCreateAPIView):
+    """This class handles the http GET, PUT and DELETE requests."""
+    serializer_class = CartSerializer
+
+    def get_queryset(self):
+        customer = self.kwargs['pk']
+        return Cart.objects.filter(customer=customer)
