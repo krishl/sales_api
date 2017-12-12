865915b9c00bfeb556f4995e8e7a7139d36ed6d0
github username <krishel.lasam@gmail.com>
2017-12-11 20:44:34 -0500

    add serializer relationships

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,13 +1,5 @@
 from rest_framework import serializers
-from .models import Customer, Product
-
-class CustomerSerializer(serializers.ModelSerializer):
-    """Serializer to map the Model instance into JSON format."""
-
-    class Meta:
-        """Meta class to map serializer's fields with the model fields."""
-        model = Customer
-        fields = ('id', 'first_name', 'last_initial')
+from .models import Customer, Product, Cart
 
 class ProductSerializer(serializers.ModelSerializer):
     """Serializer to map the Model instance into JSON format."""
@@ -16,3 +8,21 @@ class ProductSerializer(serializers.ModelSerializer):
         """Meta class to map serializer's fields with the model fields."""
         model = Product
         fields = ('id', 'name', 'description')
+
+class CartSerializer(serializers.ModelSerializer):
+    """Serializer to map the Model instance into JSON format."""
+    products = ProductSerializer(many=True, read_only=True)
+
+    class Meta:
+        """Meta class to map serializer's fields with the model fields."""
+        model = Cart
+        fields = ('id', 'customer', 'products')
+
+class CustomerSerializer(serializers.ModelSerializer):
+    """Serializer to map the Model instance into JSON format."""
+    cart = CartSerializer(many=False, read_only=True)    
+
+    class Meta:
+        """Meta class to map serializer's fields with the model fields."""
+        model = Customer
+        fields = ('id', 'first_name', 'last_initial', 'cart')
